7cb3421dcc936033cfaaa68a95090e42c1a08e1b
koen12344 <koen@koenreus.com>
2021-11-20 17:19:50 +0100

    Initial

---
+++ flexible-user-submitted-content.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Flexible User Submitted Content
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     Let visitors submit content based on multiple fields and generate a single piece of content from it
+ * Author:          Koen Reus
+ * Author URI:      https://koenreus.com
+ * Text Domain:     flexible-user-submitted-content
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Flexible_User_Submitted_Content
+ */
+
+// Your code starts here.
